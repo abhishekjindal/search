6fc44c7eaa1991161dca88c5d874afb3f394e43f
abhishekjindal <abhishekjindal93@gmail.com>
2017-06-23 23:50:54 +0530

    Added merge

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,43 @@ var router = express.Router();
 var http = require('http');
 var async = require('async');
 
+function merge(a,b){
+	var i=0, j=0;
+	var all = [];
+	var len1 = a.length, len2 = b.length;
+	var index = 0;
+	if(len1 == 0){
+		// console.log(a.concat(b));
+		all = all.concat(b);
+	}else if(len2 == 0){
+		all = all.concat(a);
+	}else{
+		while(i< len1 && j< len2){
+			if(a[i].ecstasy >= b[j].ecstasy){
+				all[index] = a[i];
+				i++;
+			}else{
+				all[index] = b[j];
+				j++;
+			}
+			index++;
+		}
+
+		while(i<len1){
+			all[index] = a[i];
+			i++;
+			index++;
+		}
+
+		while(j<len2){
+			all[index] = b[i];
+			j++;
+			index++;
+		}
+	}
+	return all;
+}
+
 /* GET home page. */
 router.get('/hotels/search', function(req, res, next) {
 
@@ -17,7 +54,8 @@ router.get('/hotels/search', function(req, res, next) {
 			http.get(scraper, function(resp){
 					resp.on('data', function(chunk){
 						// append to results array
-						results = results.concat(JSON.parse(chunk).results);
+						results = merge(results, JSON.parse(chunk).results);
+						// results = results.concat(JSON.parse(chunk).results);
 					});
 					resp.on('end', function(){
 						callback();
@@ -30,9 +68,9 @@ router.get('/hotels/search', function(req, res, next) {
 				res.send(err);
 			}
 			// sort the results with descending ecstasy
-			results.sort(function(a,b){
-				return b.ecstasy - a.ecstasy;
-			});
+			// results.sort(function(a,b){
+			// 	return b.ecstasy - a.ecstasy;
+			// });
 			// send results as requested
 			res.send({"results":results});
 		});
